228265ac2ccfe21285f5c5714812095d3231c20e
Davide Bussone <57213004+davibuss@users.noreply.github.com>
2020-11-18 14:41:39 +0100

    Update eval_run_flow.py

--- Scripts/eval_run_flow.py
+++ Scripts/eval_run_flow.py
@@ -72,7 +72,7 @@ def main_run(dataset, model_state_dict, dataset_dir, stackSize, numSeg):
     plt.savefig(dataset + '-flow.jpg', bbox_inches='tight')
     plt.show()
 
-def __main__():
+def __main__(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default='gtea61', help='Dataset')
     parser.add_argument('--datasetDir', type=str, default='./dataset/gtea_warped_flow_61/split2/test',
