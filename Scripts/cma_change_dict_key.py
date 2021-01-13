f5b6e845ba158278f571c10f7395cc17ea6ed335
filicurtes <fili.c.96@hotmail.it>
2021-01-13 14:49:03 +0100

    fix the keychanger script

--- Scripts/cma_change_dict_key.py
+++ Scripts/cma_change_dict_key.py
@@ -3,12 +3,29 @@ from ML_DL_Project.Scripts.resnetMod import *
 from ML_DL_Project.Scripts.flow_resnet import *
 
 def change_key_names(flow_model_path,frame_model_path,out_path_flow,out_path_frame):
+    
+    flowModel = flow_resnet34(False, channels=10, num_classes=61)
+    flowModel.load_state_dict(torch.load(flow_model_path))
+    flowModelB = flow_resnet34(False, channels=10, num_classes=61)
+    frameModel.load_state_dict(torch.load(flow_model_path))
+
+    state_dict = flowModel.state_dict()
+    state_dict_v2 = copy.deepcopy(state_dict)
+    for key in state_dict:
+        if 'conv1' in key:
+            pre, post = key.split('.')
+            state_dict_v2[pre+'_cm_fl'+'.'+post] = state_dict_v2.pop(key)
+
+    frameModelB.load_state_dict(state_dict_v2,strict=False)
+
+    torch.save(flowModelB.state_dict(), out_path_flow)
 
     
+    
 
-    frameModel = resnet34(num_classes, memSize)
+    frameModel = resnet34(True, True)
     frameModel.load_state_dict(torch.load(frame_model_path))
-    frameModelB = resnet34(num_classes, memSize)
+    frameModelB = resnet34(True,True)
     frameModel.load_state_dict(torch.load(frame_model_path))
 
     state_dict = frameModel.state_dict()
@@ -20,5 +37,5 @@ def change_key_names(flow_model_path,frame_model_path,out_path_flow,out_path_fra
 
     frameModelB.load_state_dict(state_dict_v2,strict=False)
 
-    torch.save(flowModelB.state_dict(), out_path_frame)
+    torch.save(frameModelB.state_dict(), out_path_frame)
 
