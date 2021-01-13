a70289f9e052413bc01e069735536b2e7313e59e
filicurtes <fili.c.96@hotmail.it>
2021-01-13 13:18:39 +0100

    fixed import for dictionary key change

--- Scripts/cma_change_dict_key.py
+++ Scripts/cma_change_dict_key.py
@@ -4,22 +4,7 @@ from ML_DL_Project.Scripts.flow_resnet import *
 
 def change_key_names(flow_model_path,frame_model_path,out_path_flow,out_path_frame):
 
-    flowModel = flow_resnet34(False, channels=2*stackSize, num_classes=num_classes)
-    flowModel.load_state_dict(torch.load(flow_model_path))
-
-    flowModelB = flow_resnet34(False, channels=2*stackSize, num_classes=num_classes)
-    flowModel.load_state_dict(torch.load(flow_model_path))
-            
-    state_dict = flowModel.state_dict()
-    state_dict_v2 = copy.deepcopy(state_dict)
-    for key in state_dict:
-        if 'conv1' in key:
-            pre, post = key.split('.')
-            state_dict_v2[pre+'_cm_fl'+'.'+post] = state_dict_v2.pop(key)
-
-    flowModelB.load_state_dict(state_dict_v2,strict=False)
-
-    torch.save(flowModelB.state_dict(), out_path_flow)
+    
 
     frameModel = attentionModel(num_classes, memSize)
     frameModel.load_state_dict(torch.load(frame_model_path))
