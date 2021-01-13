be561fea1a1317b713aec64a60f02bb6b31d0783
filicurtes <fili.c.96@hotmail.it>
2021-01-13 14:50:52 +0100

    fix the keychanger script

--- Scripts/cma_change_dict_key.py
+++ Scripts/cma_change_dict_key.py
@@ -7,7 +7,7 @@ def change_key_names(flow_model_path,frame_model_path,out_path_flow,out_path_fra
     flowModel = flow_resnet34(False, channels=10, num_classes=61)
     flowModel.load_state_dict(torch.load(flow_model_path))
     flowModelB = flow_resnet34(False, channels=10, num_classes=61)
-    frameModel.load_state_dict(torch.load(flow_model_path))
+    flowModel.load_state_dict(torch.load(flow_model_path))
 
     state_dict = flowModel.state_dict()
     state_dict_v2 = copy.deepcopy(state_dict)
@@ -16,7 +16,7 @@ def change_key_names(flow_model_path,frame_model_path,out_path_flow,out_path_fra
             pre, post = key.split('.')
             state_dict_v2[pre+'_cm_fl'+'.'+post] = state_dict_v2.pop(key)
 
-    frameModelB.load_state_dict(state_dict_v2,strict=False)
+    flowModelB.load_state_dict(state_dict_v2,strict=False)
 
     torch.save(flowModelB.state_dict(), out_path_flow)
 
