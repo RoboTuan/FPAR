6b54ddca69db58e8dc9a973053c648cd86638413
filicurtes <fili.c.96@hotmail.it>
2021-01-15 17:02:10 +0100

    major update on flow feats avg

--- Scripts/cma_doubleRes_p_LSTM.py
+++ Scripts/cma_doubleRes_p_LSTM.py
@@ -31,7 +31,8 @@ class doubleresnet_lstm_Model(nn.Module):
         self.lstm_cell = MyConvLSTMCell(512, mem_size)
         # Here I initialize another avgpool needed after the convLSTM
         self.avgpool = nn.AvgPool2d(7)
-        self.conv1_1 = nn.Conv2d(seqLen, 1, kernel_size=1, stride = 1, padding =1 , bias=False)
+        #self.conv1_1 = nn.Conv2d(seqLen, 1, kernel_size=1, stride = 1, padding =1 , bias=False)
+        self.avgpool_flow = nn.AvgPool3d((seLen,1))
         self.dropout = nn.Dropout(0.7)
         # Here I initialize the last classifier
         self.fc = nn.Linear(mem_size, self.num_classes)
@@ -47,7 +48,7 @@ class doubleresnet_lstm_Model(nn.Module):
             # The logits are the result of the last layer of the cnn (the raw ouput) without softmax
             # Pass the image to the resnet and get back the featuremap at the end of the resnet in "logit"
             # get returned in feature_conv and feature_convNBN the features map of the 4th layer of the resnet
-            logit, feature_conv, feature_convNBN, flow_features = self.resNet(inputVariableFlow[t],inputVariableFrame[t])
+            logit, feature_conv, feature_convNBN, flow_features = self.resNet(inputVariableFlow[t],inputVariableFrame)
             #stack the flow features along a new dimension:
             flow_features_maps.append(flow_features)
             bz, nc, h, w = feature_conv.size()
@@ -77,6 +78,6 @@ class doubleresnet_lstm_Model(nn.Module):
             state = self.lstm_cell(attentionFeat, state)
         feats1 = self.avgpool(state[1]).view(state[1].size(0), -1)
         flow_feats = torch.stack(flow_features_maps,0)
-        flow_feats = self.conv1_1(flow_feats)
+        flow_feats = self.avgpool_flow(flow_feats)
         feats = self.classifier(feats1)
         return feats, feats1, flow_feats
