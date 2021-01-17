5e4912fa41d7424e229d8ac28d7df1835938ac5a
Davide Bussone <57213004+davibuss@users.noreply.github.com>
2021-01-17 17:26:13 +0100

    Update with flow self sup

--- Scripts/eval_run_ss.py
+++ Scripts/eval_run_ss.py
@@ -2,13 +2,15 @@ from __future__ import print_function, division
 from ML_DL_Project.Scripts.spatial_transforms import (Compose, ToTensor, CenterCrop, Scale, Normalize, MultiScaleCornerCrop,
                                 RandomHorizontalFlip)
 from ML_DL_Project.Scripts.makeMmaps import *
+from ML_DL_Project.Scripts.makeDatasetTwoStream import *
+
 from ML_DL_Project.Scripts.SelfSupObjectAttentionModelConvLSTM import *
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import argparse
 import sys
 
-def main_run(dataset, model_state_dict, dataset_dir, seqLen, memSize, regression):
+def main_run(dataset, model_state_dict, dataset_dir, seqLen, stackSize, memSize, regression, Flow):
 
     if dataset == 'gtea61':
         num_classes = 61
@@ -30,16 +32,24 @@ def main_run(dataset, model_state_dict, dataset_dir, seqLen, memSize, regression
     normalize = Normalize(mean=mean, std=std)
     spatial_transform = Compose([Scale(256), CenterCrop(224), ToTensor(), normalize])
 
-    vid_seq_test = makeDataset(dataset_dir,
+    
+    
+
+    if Flow == True:
+      vid_seq_test = makeDataset2Stream(dataset_dir, spatial_transform=spatial_transform, stackSize=stackSize, seqLen=seqLen, fmt='.png', selfSup=True)
+    else:
+      vid_seq_test = makeDataset(dataset_dir,
                                spatial_transform=spatial_transform,
                                seqLen=seqLen, fmt='.png')
+      
     actions =vid_seq_test.__getLabel__()
 
+
     test_loader = torch.utils.data.DataLoader(vid_seq_test, batch_size=1,
-                            shuffle=False, num_workers=2, pin_memory=True)
+                            shuffle=False, num_workers=2, pin_memory=True,drop_last=True)
     
     
-    model = SelfSupAttentionModel(num_classes=num_classes, mem_size=memSize, REGRESSOR=regression)
+    model = SelfSupAttentionModel(num_classes=num_classes, mem_size=memSize, REGRESSOR=regression,Flow=Flow)
     
     model.load_state_dict(torch.load(model_state_dict))
 
@@ -63,6 +73,9 @@ def main_run(dataset, model_state_dict, dataset_dir, seqLen, memSize, regression
         inputMmap = inputMmap.to(DEVICE)
         output_label, _ , mmapPrediction = model(inputVariable)
 
+        if Flow == True:
+          inputMmap = torch.reshape(inputMmap, (1, 14, 1, 7, 7))
+        
         if regression==True:
           mmapPrediction = mmapPrediction.view(-1)
           #Regression -> float number for the input motion maps
@@ -88,20 +101,34 @@ def main_run(dataset, model_state_dict, dataset_dir, seqLen, memSize, regression
 
     cnf_matrix = confusion_matrix(true_labels, predicted_labels).astype(float)
     cnf_matrix_normalized = cnf_matrix / cnf_matrix.sum(axis=1)[:, np.newaxis]
+    if Flow ==True:
 
+      ticks = [str(action + str(i) ) for i, action in enumerate(actions)]
+      plt.figure(figsize=(20,20))
+      plt.imshow(cnf_matrix_normalized, interpolation='none', cmap='YlOrRd')
+      plt.colorbar()
+      plt.xticks(np.arange(num_classes),labels = set(ticks), fontsize=10, rotation = 90)
+      plt.yticks(np.arange(num_classes),labels = set(ticks), fontsize=10)
+      plt.xlabel("Predicted")
+      plt.ylabel("True")
+      plt.grid(True)
+      plt.clim(0, 1)
+      plt.savefig(dataset + '-selfSupFlow.jpg', bbox_inches='tight')
+      plt.show()
+    else:
+      ticks = [str(action + str(i) ) for i, action in enumerate(actions)]
+      plt.figure(figsize=(20,20))
+      plt.imshow(cnf_matrix_normalized, interpolation='none', cmap='Reds')
+      plt.colorbar()
+      plt.xticks(np.arange(num_classes),labels = set(ticks), fontsize=10, rotation = 90)
+      plt.yticks(np.arange(num_classes),labels = set(ticks), fontsize=10)
+      plt.xlabel("Predicted")
+      plt.ylabel("True")
+      plt.grid(True)
+      plt.clim(0, 1)
+      plt.savefig(dataset + '-selfSup.jpg', bbox_inches='tight')
+      plt.show()
 
-    ticks = [str(action + str(i) ) for i, action in enumerate(actions)]
-    plt.figure(figsize=(20,20))
-    plt.imshow(cnf_matrix_normalized, interpolation='none', cmap='Reds')
-    plt.colorbar()
-    plt.xticks(np.arange(num_classes),labels = set(ticks), fontsize=10, rotation = 90)
-    plt.yticks(np.arange(num_classes),labels = set(ticks), fontsize=10)
-    plt.xlabel("Predicted")
-    plt.ylabel("True")
-    plt.grid(True)
-    plt.clim(0, 1)
-    plt.savefig(dataset + '-selfSup.jpg', bbox_inches='tight')
-    plt.show()
 
 def __main__(argv=None):
     parser = argparse.ArgumentParser(prog='myprogram', description='Foo')
@@ -114,6 +141,7 @@ def __main__(argv=None):
     parser.add_argument('--stackSize', type=int, default=5, help='Number of opticl flow images in input')
     parser.add_argument('--memSize', type=int, default=512, help='ConvLSTM hidden state size')
     parser.add_argument('--regression', type=bool, default=True, help='Do the motion segmentation task (selfSup) with regression ')
+    parser.add_argument('--Flow', type=bool, default=True, help='Use warp flow or improved dense trajectories ')
 
 
     args, _ = parser.parse_known_args(argv)
@@ -122,8 +150,9 @@ def __main__(argv=None):
     model_state_dict = args.model_state_dict
     dataset_dir = args.datasetDir
     seqLen = args.seqLen
+    stackSize = args.stackSize
     memSize = args.memSize
     regression= args.regression
+    Flow= args.Flow
 
-
-    main_run(dataset, model_state_dict, dataset_dir, seqLen, memSize, regression)
+    main_run(dataset, model_state_dict, dataset_dir, seqLen, memSize, regression, Flow)
