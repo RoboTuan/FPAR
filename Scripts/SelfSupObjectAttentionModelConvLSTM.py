ce1d8c180fb8db04e9daa29edb1465c938fb3f8a
RoboTuan <testingapplication96.@gmail.com>
2021-01-27 15:44:00 +0100

    Asas

--- Scripts/SelfSupObjectAttentionModelConvLSTM.py
+++ Scripts/SelfSupObjectAttentionModelConvLSTM.py
@@ -107,7 +107,7 @@ class SelfSupAttentionModel(nn.Module):
         idt = torch.reshape(idt, (7,7))
         print(idt.shape)
         fig = plt.figure()
-        plt.imshow(idt.cpu().detach().numpy())
+        plt.imshow(idt.cpu().detach().numpy(), cmap="gray")
         sys.exit()
         # FINE IMPORTANTE
 
